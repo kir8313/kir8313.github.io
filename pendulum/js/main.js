9e9b6a238736ed613c46a095a3ba3d6f832ce141
Кирилл <73424187+kir8313@users.noreply.github.com>
2021-05-16 21:50:30 +0300

    Правки маятник
    убрал скачок у пружины при возврате на исходную.
    Анимация фона у инпутов в формуле, при сбросе.

--- pendulum/js/main.js
+++ pendulum/js/main.js
@@ -6,84 +6,77 @@ function pendulum() {
   const x0 = document.querySelector('#input-way');
   var requestId = null
 
-  // кнопка запуск
-  btnOn();
-  function btnOn() {
-    document.querySelector('.pendulum-demo__btn-start').addEventListener('click', function() {  
-      // скрываю кнопку, показываю стоп.  
-      this.setAttribute('hidden', '');
-      document.querySelector('.pendulum-demo__btn-stop').removeAttribute('hidden');
-      // отслеживаю изменение пружины и передаю значение шару
-      function ballMoving() {
-        let position = document.querySelector('.pendulum-demo__svg-spring').getBoundingClientRect().height;
-        document.querySelector('.pendulum-demo__svg-circle').style.setProperty('--y', position + "px");  
-        requestId = requestAnimationFrame(ballMoving) 
-      }
-      // для плавной анимации
-      requestAnimationFrame(ballMoving);     
-      // передаю параметры в формулу
-      document.querySelector('#weight').value = weight.value;
-      document.querySelector('#stiffness').value = stiffness.value;
-      document.querySelector('#x0').value = x0.value;         
-      // высчитываю частоту колебаний, округляю, передаю в cubic-bezier
-      let answer = (document.querySelector('#x0').value / 100) * 
-                    (Math.sqrt(document.querySelector('#stiffness').value /
-                    document.querySelector('#weight').value));
-      document.querySelector('#answer').value = answer.toFixed(2);
-      document.querySelector('.pendulum-demo__svg-spring').style.setProperty('--cubic', answer.toFixed(2))
-      // запускаю анимацию фона полей в формуле
-      showBgInput();
-      // после задержки 1сек передаю в переменные:  размера шара, х0, макс. и мин. растягивание пружины
-      timeout1 = setTimeout(()=> {
-        let circleWidth = Math.round(((Math.cbrt(weight.value)) * 100 / 2)) ;
-        document.querySelector(':root').style.setProperty('--circle-width', circleWidth +"px");  
-        document.querySelector('.pendulum-demo__svg-start-position').style.setProperty('--x0', x0.value + "px");  
-        document.querySelector(':root').style.setProperty('--scale-more',  1 + x0.value / 100 );  
-        document.querySelector(':root').style.setProperty('--scale-less',  1 - x0.value / 100 );  
-      }, 1000)
-
-      // после задержки 2с запускаю анимацию
-      timeout2 = setTimeout(() => {
-        document.querySelector('.pendulum-demo__svg').classList.add('--anim');
-        stopWatch();
-      }, 2000)
-    })
-  }
-  // кнопка отключения
-  btnOff()
-  function btnOff() {
-      document.querySelector('.pendulum-demo__btn-stop').addEventListener('click', function() {
-        // скрыл кнопку, показал старт
-        this.setAttribute('hidden', '')
-        document.querySelector('.pendulum-demo__btn-start').removeAttribute('hidden');
-        // вернул исходные значения ширины круга и позицию
-        document.querySelector(':root').style.setProperty('--circle-width', 40 +"px"); 
-        document.querySelector('.pendulum-demo__svg-circle').style.setProperty('--y', 129 +"px"); 
-        // сбросил обработку анимации
-        cancelAnimationFrame(requestId); 
-        // сбросил секундомер
-        ClearСlock();
-        // сбросил тайм ауты и убрал класс анимирующий
-        clearTimeout(timeout1);
-        clearTimeout(timeout2);
-        clearTimeout(timeout3);
-        clearTimeout(timeout4);
-        document.querySelector('.pendulum-demo__svg').classList.remove('--anim');
-        
-      })
+  // кнопка старт
+  document.querySelector('.pendulum-demo__btn-start').addEventListener('click', function btnOn() {  
+    // скрываю кнопку, показываю стоп.  
+    this.setAttribute('hidden', '');
+    document.querySelector('.pendulum-demo__btn-stop').removeAttribute('hidden');
+    // отслеживаю изменение пружины и передаю значение шару
+    function ballMoving() {
+      let position = document.querySelector('.pendulum-demo__svg-spring').getBoundingClientRect().height;
+      document.querySelector('.pendulum-demo__svg-circle').style.setProperty('--y', position + "px");  
+      requestId = requestAnimationFrame(ballMoving) 
     }
-   
+    // для плавной анимации
+    requestAnimationFrame(ballMoving);     
+    // передаю параметры в формулу
+    document.querySelector('#weight').value = weight.value;
+    document.querySelector('#stiffness').value = stiffness.value;
+    document.querySelector('#x0').value = x0.value;         
+    // высчитываю частоту колебаний, округляю, передаю в cubic-bezier
+    let answer = (document.querySelector('#x0').value / 100) * 
+                  (Math.sqrt(document.querySelector('#stiffness').value /
+                  document.querySelector('#weight').value));
+    document.querySelector('#answer').value = answer.toFixed(2);
+    document.querySelector('.pendulum-demo__svg-spring').style.setProperty('--cubic', answer.toFixed(2))
+    // запускаю анимацию фона полей в формуле
+    showBgInput();
+    // после задержки 1сек передаю в переменные:  размера шара, х0, макс. и мин. растягивание пружины
+    timeout1 = setTimeout(()=> {
+      let circleWidth = Math.round(((Math.cbrt(weight.value)) * 100 / 2)) ;
+      document.querySelector(':root').style.setProperty('--circle-width', circleWidth +"px");  
+      document.querySelector('.pendulum-demo__svg-start-position').style.setProperty('--x0', x0.value + "px");  
+      document.querySelector(':root').style.setProperty('--scale-more',  1 + x0.value / 100 );  
+      document.querySelector(':root').style.setProperty('--scale-less',  1 - x0.value / 100 );  
+    }, 1000)
 
+    // после задержки 2с запускаю анимацию
+    timeout2 = setTimeout(() => {
+      document.querySelector('.pendulum-demo__svg').classList.add('--anim');
+      stopWatch();
+    }, 2000)
+  })
+
+  // кнопка отключения
+  document.querySelector('.pendulum-demo__btn-stop').addEventListener('click', function btnOff() {
+    // скрыл кнопку, показал старт
+    this.setAttribute('hidden', '')
+    document.querySelector('.pendulum-demo__btn-start').removeAttribute('hidden');
+    // вернул исходные значения ширины круга и позицию
+    document.querySelector(':root').style.setProperty('--circle-width', 40 +"px"); 
+    document.querySelector('.pendulum-demo__svg-circle').style.setProperty('--y', 129 +"px"); 
+    // сбросил обработку анимации
+    cancelAnimationFrame(requestId); 
+    // сбросил секундомер
+    ClearСlock();
+    // сбросил тайм ауты и убрал класс анимирующий
+    clearTimeout(timeout1);
+    clearTimeout(timeout2);
+    clearTimeout(timeout3);
+    clearTimeout(timeout4);
+    document.querySelector('.pendulum-demo__svg').classList.remove('--anim');
+    showBgInput();
+  })
 
   // для анимации фона у инпутов в формуле и тайм аут на удаление класса
   function showBgInput() {
     document.querySelectorAll('.pendulum-result__formula>input').forEach(input => {
       input.classList.add('change')
-      timeout3 = setTimeout(() => input.classList.remove('change'), 1500)
+      timeout3 = setTimeout(() => input.classList.remove('change'), 1000)
     })  
     document.querySelectorAll('.pendulum-result__formula-fraction>input').forEach(input => {
       input.classList.add('change')
-      timeout4 = setTimeout(() => input.classList.remove('change'), 1500)
+      timeout4 = setTimeout(() => input.classList.remove('change'), 1000)
     })  
   }
   // задал рамки на максимальную величину и минимальную у параметров
